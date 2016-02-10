7ec7aa787fcf2ac74a17affb56c69d98b50b83e8
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2016-02-10 13:47:06 +0530

    Upgrade to newer apache httpclient, supports SNI used by some web servers

--- yum-plugin/src/com/tw/go/plugin/material/artifactrepository/yum/exec/PackageRepositoryPoller.java
+++ yum-plugin/src/com/tw/go/plugin/material/artifactrepository/yum/exec/PackageRepositoryPoller.java
@@ -1,18 +1,18 @@
-/*************************GO-LICENSE-START*********************************
- * Copyright 2014 ThoughtWorks, Inc.
+/*
+ * Copyright 2016 ThoughtWorks, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *************************GO-LICENSE-END***********************************/
+ */
 package com.tw.go.plugin.material.artifactrepository.yum.exec;
 
 import com.thoughtworks.go.plugin.api.logging.Logger;
@@ -25,8 +25,8 @@ import org.apache.commons.codec.digest.DigestUtils;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.tw.go.plugin.common.util.ListUtil.join;
 import static java.util.Arrays.asList;
-import static org.apache.commons.lang.StringUtils.join;
 
 public class PackageRepositoryPoller {
 
--- yum-plugin/test/com/tw/go/plugin/material/artifactrepository/yum/exec/HttpConnectionCheckerTest.java
+++ yum-plugin/test/com/tw/go/plugin/material/artifactrepository/yum/exec/HttpConnectionCheckerTest.java
@@ -1,96 +1,104 @@
-/*************************GO-LICENSE-START*********************************
- * Copyright 2014 ThoughtWorks, Inc.
+/*
+ * Copyright 2016 ThoughtWorks, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *************************GO-LICENSE-END***********************************/
+ */
 
 package com.tw.go.plugin.material.artifactrepository.yum.exec;
 
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpMethod;
-import org.apache.commons.httpclient.HttpStatus;
-import org.apache.commons.httpclient.StatusLine;
-import org.apache.commons.httpclient.methods.GetMethod;
+import okhttp3.HttpUrl;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Matchers;
 
 import java.io.IOException;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
 
 public class HttpConnectionCheckerTest {
 
     private HttpConnectionChecker checker;
+    private MockWebServer webServer;
 
     @Before
     public void setUp() throws Exception {
-        checker = spy(new HttpConnectionChecker());
+        webServer = new MockWebServer();
+        checker = new HttpConnectionChecker();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        webServer.shutdown();
     }
 
     @Test
-    public void shouldNotThrowExceptionIfCheckConnectionToTheRepoPasses() throws IOException {
-        HttpClient httpClient = mock(HttpClient.class);
-        when(checker.getHttpClient()).thenReturn(httpClient);
-        GetMethod getMethod = mock(GetMethod.class);
-        when(checker.getGetMethod(Matchers.<String>any())).thenReturn(getMethod);
-        when(httpClient.executeMethod(getMethod)).thenReturn(HttpStatus.SC_OK);
+    public void shouldNotThrowExceptionIfCheckConnectionToTheRepoPasses() throws Exception {
+        webServer.enqueue(new MockResponse().setResponseCode(200).setBody(""));
+        HttpUrl url = webServer.url("/repodata/repomd.xml");
 
-        checker.checkConnection("url", new Credentials(null, null));
+        checker.checkConnection(url.toString(), new Credentials(null, null));
 
-        verify(httpClient).executeMethod(getMethod);
-        verify(getMethod, never()).getStatusLine();
+        RecordedRequest recordedRequest = webServer.takeRequest();
+        assertEquals("/repodata/repomd.xml", recordedRequest.getPath());
+        assertEquals(1, webServer.getRequestCount());
     }
+
+    @Test
+    public void shouldPerformBasicAuthUsingChallengeResponseAuth() throws Exception {
+        webServer.enqueue(new MockResponse().setResponseCode(401).setHeader("WWW-Authenticate", "Basic realm=\"YumRepo\""));
+        webServer.enqueue(new MockResponse().setResponseCode(200).setBody(""));
+
+        HttpUrl url = webServer.url("/repodata/repomd.xml");
+
+        checker.checkConnection(url.toString(), new Credentials("foo", "bar"));
+        assertEquals(2, webServer.getRequestCount());
+
+        RecordedRequest recordedRequest = webServer.takeRequest();
+        assertEquals("/repodata/repomd.xml", recordedRequest.getPath());
+        assertThat(recordedRequest.getHeaders().get("Authorization"), is((String) null));
+
+        recordedRequest = webServer.takeRequest();
+        assertEquals("/repodata/repomd.xml", recordedRequest.getPath());
+        assertThat(recordedRequest.getHeaders().get("Authorization"), is("Basic Zm9vOmJhcg=="));
+    }
+
 
     @Test
     public void shouldFailCheckConnectionToTheRepoWhenHttpClientReturnsAUnSuccessfulReturnCode() throws IOException {
-        HttpClient httpClient = mock(HttpClient.class);
-        GetMethod getMethod = mock(GetMethod.class);
-        when(checker.getHttpClient()).thenReturn(httpClient);
-        when(checker.getGetMethod(Matchers.<String>any())).thenReturn(getMethod);
-        when(httpClient.executeMethod(getMethod)).thenReturn(HttpStatus.SC_UNAUTHORIZED);
-        when(getMethod.getStatusLine()).thenReturn(new StatusLine("HTTP/1.1 401 Unauthorized"))    ;
+        webServer.enqueue(new MockResponse().setResponseCode(500));
+        HttpUrl url = webServer.url("/repodata/repomd.xml");
 
         try {
-            checker.checkConnection("url", new Credentials(null, null));
+            checker.checkConnection(url.toString(), new Credentials(null, null));
             fail("should fail");
         } catch (Exception e) {
-            assertThat(e.getMessage(), is("HTTP/1.1 401 Unauthorized"));
+            assertThat(e.getMessage(), is("HTTP/1.1 500 Server Error"));
         }
-        verify(httpClient).executeMethod(getMethod);
-        verify(getMethod).getStatusLine();
+        assertEquals(1, webServer.getRequestCount());
     }
 
     @Test
     public void shouldFailCheckConnectionToTheRepoWhenHttpClientThrowsIOException() throws IOException {
-        HttpClient httpClient = mock(HttpClient.class);
-        GetMethod getMethod = mock(GetMethod.class);
-        when(checker.getHttpClient()).thenReturn(httpClient);
-        when(checker.getGetMethod(Matchers.<String>any())).thenReturn(getMethod);
-        when(httpClient.executeMethod(getMethod)).thenThrow(new IOException("some i/o error occurred"));
-
         try {
-            checker.checkConnection("url", new Credentials(null, null));
+            checker.checkConnection("https://localhost:11111", new Credentials(null, null));
             fail("should fail");
         } catch (Exception e) {
-            assertThat(e.getMessage(), is("java.io.IOException: some i/o error occurred"));
+            assertThat(e.getMessage(), containsString("Connection refused"));
         }
-        verify(httpClient).executeMethod(getMethod);
-        verify(getMethod, never()).getStatusLine();
     }
 }
